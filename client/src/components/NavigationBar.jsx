af1c55579ecc06d2ba5290d365b93114cb5bcaa2
Amy Paulin <mimi@Amys-MacBook-Air-4.local>
2026-03-26 00:15:43 -0500

    last one for tdy

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -10,7 +10,6 @@ export default function NavigationBar() {
 
   return (
     <nav className="bg-white shadow-md px-12 py-4 flex justify-between items-center">
-
       {/*library logo in the right*/}
       <div
         onClick={() => navigate(homeRoute)}
@@ -18,28 +17,7 @@ export default function NavigationBar() {
       >
         Library Database
       </div>
-
-      {/*admin dashboard button*/}
-      <div className="flex items-center gap-3">
-        {isAdmin && (
-          <button
-            onClick={() => navigate("/admin")}
-            className="bg-gray-800 text-white px-4 py-2 rounded-lg text-sm font-semibold hover:bg-gray-900"
-          >
-            Admin Dashboard
-          </button>
-        )}
-        
-        {/*staff dashboard button*/}
-        {isStaff && (
-          <button
-            onClick={() => navigate("/staff")}
-            className="bg-gray-800 text-white px-4 py-2 rounded-lg text-sm font-semibold hover:bg-gray-900"
-          >
-            Staff Dashboard
-          </button>
-        )}
-
+      
         {/*log out button */}
         <button
           onClick={() => {
@@ -50,7 +28,6 @@ export default function NavigationBar() {
         >
           Log Out ➜]
         </button>
-      </div>
     </nav>
   );
 }
